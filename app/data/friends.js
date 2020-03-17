37a91c0a9e1b7f3b92c27d3a3c9840e9872e7f7e
Mlipphardt <matthewlipphardt@gmail.com>
2020-03-17 19:35:26 -0400

    Survey now successfully picks candidate from api with the lowest total difference.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,8 @@
-var friendsArray = [];
+var friendsArray = [{
+    name: "Matthew",
+    photo: "www.xkcd.com",
+    score: [1, 4, 5, 4, 4, 3, 5, 2, 2, 2]
+}];
   
 module.exports = friendsArray;
   
